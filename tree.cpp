dc29a11f05fceb9aca3d3bc0f97855bf28a0d9b4
stalord63 <adi06031998@gmail.com>
2022-07-26 22:47:46 +0530

    level order

--- tree.cpp
+++ tree.cpp
@@ -28,3 +28,37 @@ root->left->right=new Node(5);
 preorder(root);
 	return 0;
 }
+//-------------------------------------------------------------------------------------------------------------
+//Level order traversal (Leetcode 102)
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;  //tree ka node store in a queue,so tree type ka queue
+        
+        if(root==NULL){
+            return ans;
+        }
+        q.push(root);  //queue me dal rhe root ko.
+        while(!q.empty()){
+            vector<int> level;
+           int size=q.size();
+            for(int i=0;i<size;i++){   //el level me itereate krega jitna element hoga
+                TreeNode* node= q.front();  
+                q.pop();
+                if(node->left!=NULL)q.push(node->left);
+                    if(node->right!=NULL)q.push(node->right);
+                        
+                        level.push_back(node->val);
+                    
+                
+            }
+            ans.push_back(level);
+            
+            
+            
+            
+        }
+        return ans;
+    }
+};
